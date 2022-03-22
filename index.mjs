c58e5178d6c83ab4554e3284f682f1b0246910b5
Valentina <matsulevichvalentina@gmail.com>
2022-03-22 18:37:43 +0300

    add: task_3/2

---
+++ index.mjs
@@ -0,0 +1,39 @@
+import sharp from 'sharp'
+import process from 'process'
+import fs from 'fs'
+import path from 'path'
+
+const input = process.argv[3]
+const output = process.argv[4]
+
+fs.readdir(output, (err, files) => {
+  if (err) throw err
+
+  for(let file of files) {
+    fs.unlink(path.join(output, file), err => {
+      if (err) throw err
+    })
+  }
+})
+
+const numb = process.argv[2].split('x')
+const width = +numb[0]
+const height = +numb[1]
+
+const filterrrayImages = () => {
+  const array = fs.readdirSync(input)
+  return array.filter(file => {
+    return (path.extname(file).toLowerCase() === '.jpg' ||
+    path.extname(file).toLowerCase() === '.png')
+  })
+}
+
+filterrrayImages().map(imageName => {
+  const image = fs.readFileSync(path.resolve(input, imageName))
+  
+  sharp(image)
+  .resize(width, height)
+  .toFile(path.resolve(output, imageName), (err) => {
+    console.log(err)
+  });
+})
