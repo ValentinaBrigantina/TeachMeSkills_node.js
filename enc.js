a293de81a5bdabb55eb0eebbb8f5f9cb1c994d2a
Valentina <va@MacBook-Pro-Valerii.local>
2022-03-28 22:17:49 +0300

    add task_4-2

---
+++ enc.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const readline = require('readline')
+const os = require('os')
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('output.txt'),
+    crlfDelay: Infinity
+})
+
+const defaultHandler = err => {
+    if (err) {
+        throw new Error(err)
+    }
+}
+
+if (fs.existsSync('result.txt')) {
+    fs.truncate('result.txt', 0, defaultHandler)
+}
+
+rl.on('line', (line) => {
+    const data = Buffer.from(line, 'hex') + os.EOL
+    const options = { flag: 'a+'}
+    fs.writeFileSync('result.txt', data, options)
+})
